cddaefc2cae5f7d83f4b7cfbb840a676fa40046f
westkitty <andrew.dolby@westcat.ca>
2026-06-08 05:08:47 -0400

    feat(ui): render shell tools through component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,8 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { GlobalDropUpload } from './components/GlobalDropUpload';
-import { ShortcutHelp } from './components/ShortcutHelp';
-import { ThemePicker } from './components/ThemePicker';
+import { ShellTools } from './components/ShellTools';
 import { ToastViewport } from './components/ToastViewport';
 import { applyStoredTheme } from './lib/themes';
 import './index.css';
@@ -16,10 +15,7 @@ applyStoredTheme();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <div className="header-tools-shell" aria-label="Display and help tools">
-      <ThemePicker />
-      <ShortcutHelp />
-    </div>
+    <ShellTools />
     <GlobalDropUpload />
     <ToastViewport />
   </StrictMode>
